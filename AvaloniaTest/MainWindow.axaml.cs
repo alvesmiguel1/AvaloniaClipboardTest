e76fd34182070c70d36c7aa142d1bd75ac6b2848
alvesmiguel1 <miguel.alves@outsystems.com>
2020-11-19 12:10:08 +0000

    Initial commit

---
+++ AvaloniaTest/MainWindow.axaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaTest {
+
+    public class MainWindow : Window {
+
+        private const string RandomFormat = "Random Format";
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void OnCopyButtonClick(object sender, RoutedEventArgs e) {
+            var bArr = new byte[20];
+            new Random().NextBytes(bArr);
+
+            var dataObj = new DataObject();
+            dataObj.Set(RandomFormat, bArr);
+
+            Application.Current.Clipboard.SetDataObjectAsync(dataObj).ContinueWith(t => {
+                Console.WriteLine("Copy task completed");
+                return t.IsCompleted;
+            });
+        }
+
+        private void OnPasteButtonClick(object sender, RoutedEventArgs e) {
+            var result = Application.Current.Clipboard.GetDataAsync(RandomFormat).Result as byte[];
+            Console.WriteLine("Clipboard is " + (result == null ? "null" : "not null") + "!");
+        }
+    }
+}
